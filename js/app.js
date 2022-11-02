d3920f974f86a0782744efd0f51842df7fce8b3d
MirkoFarina <MirkoFarina@outlook.com>
2022-11-02 14:39:37 +0100

    [add] all bonus

--- js/app.js
+++ js/app.js
@@ -1,19 +1,14 @@
-/* Descrizione:
-Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-Utilizzare almeno una classe CSS in modo dinamico
-Bonus:
-1. Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-2. Cambiare dinamicamente l’immagine
-3. Al click di un bottone nascondere l’immagine e mostrare un testo */
 const { createApp } = Vue;
 
 createApp({
     data(){
         return {
-            messaggio: 'Ciao Vue!',
+            messaggio: 'Ciao Snowboarder!',
             colorText: 'red',
             percorsoImg: 'img/',
-            img: 'snow.jpg'
+            img: 'snow.jpg',
+            secondImg: 'snow2.webp',
+            isShow: false
         }
     }
 }).mount('#app')
