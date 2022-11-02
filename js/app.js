cd41c4916b41c64060087b4fe14e9be89616c380
MirkoFarina <MirkoFarina@outlook.com>
2022-11-02 14:57:58 +0100

    [add] change img loop

--- js/app.js
+++ js/app.js
@@ -10,5 +10,15 @@ createApp({
             secondImg: 'snow2.webp',
             isShow: false
         }
+    },
+    methods: {
+        changeImg(){
+            if (this.img === this.secondImg) {
+                this.img = 'snow.jpg';
+            }else {
+                this.img = this.secondImg;
+            }
+
+        }
     }
 }).mount('#app')
